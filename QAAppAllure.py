0f67f23d618df6a529e4b9cc3bb225d5a995f688
Luis Sepulveda <luis.sepulveda+omcitcld@credera.com>
2025-06-16 14:16:19 +0200

    Testing 1 model BFV1

--- QAAppAllure.py
+++ QAAppAllure.py
@@ -126,7 +126,7 @@ manual_test_cases = [
 
     
     
-    {"test_name": "BFV1", "market_code": "AT/de"},
+    {"test_name": "BFV1", "market_code": "AT/de", "model_code": "H247"},
 
         
         
